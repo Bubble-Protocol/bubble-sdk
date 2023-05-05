a66be183847bdef3b5d4f07daff89dab487f05d5
Dave.Potter <dave@datonalabs.org>
2023-05-05 12:05:29 +0100

    improve doc comments in contract code

--- contracts/AccessControlBits.sol
+++ contracts/AccessControlBits.sol
@@ -1,11 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @dev Access control bits, as returned by the AccessControlledStorage getAccessPermissions 
+ * method.
+ * 
+ * Permissions are a 256 bit field formatted with posix-like file permissions.  Only the top
+ * 16 most significant bits are reserved for Bubble Protocol.  The rest are free for
+ * application use.
+ * 
+ *   Bit    Purpose
+ *   -----  -------------------------------------------------
+ *   255       terminated (1 = bubble terminated, 0 = active)
+ *   254       directory
+ *   253       read
+ *   252       write
+ *   251       append
+ *   250       execute
+ *   240..249  reserved for Bubble Protocol future extension
+ *   0..239    user-defined
+ */
+
+// Bits
 uint constant BUBBLE_TERMINATED_BIT = 1 << 255;
 uint constant DIRECTORY_BIT = 1 << 254;
 uint constant READ_BIT = 1 << 253;
 uint constant WRITE_BIT = 1 << 252;
 uint constant APPEND_BIT = 1 << 251;
 uint constant EXECUTE_BIT = 1 << 250;
-uint constant ALL_PERMISSIONS = 15 << 250;
+
+// Combinations
 uint constant NO_PERMISSIONS = 0;
+uint constant RWA_BITS = 7 << 251;
+uint constant DRWA_BITS = 15 << 251;
+uint constant RWAX_BITS = 15 << 250;
--- contracts/BubbleStandardContent.sol
+++ contracts/BubbleStandardContent.sol
@@ -6,8 +6,7 @@ pragma solidity ^0.8.0;
  * to be discoverable.  The directory can hold any public files, such as images, videos, html, that are
  * referenced in the metadata.
  * 
- * (Content id is the keccak256 hash of the string `Bubble Standard Public Discovery Directory`)
+ * (Content id is the keccak256 hash of the string `>>Bubble Standard Public Discovery Directory<<`)
  */
-uint constant PUBLIC_DISCOVERY_DIRECTORY = 0x0f803327fe9dad88d28b7e1faa8f3cc82cae48b3e1be533d2528e1ab3376fa7c;
-
+uint constant PUBLIC_DISCOVERY_DIRECTORY = 0x2f472e7e86c65cd883cfacd8b568266b7a971b0d46620e0c6849ccfd50af1148;
 
