20cbb7993c91fdd59d23d71b75ac56f9b9dbc08a
Dave.Potter <dave@datonalabs.org>
2023-05-10 11:20:52 +0100

    add hierarchy of index.js

---
+++ packages/client/src/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Bubble Protocol
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+export {Bubble} from './Bubble';
+export {ContentManager, BubbleContentManager} from './ContentManager';
+export {EncryptionPolicy} from './EncryptionPolicy';
+export {bubbleProviders} from './bubble-providers';
+export {encryptionPolicies} from './encryption-policies';
--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -2,10 +2,5 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-/**
- * Bubble-SDK main entry point
- */
-
-export * from './core/src/index';
-export * from './client/src/index';
-export * from './server/src/index';
+export {Guardian} from './Guardian';
+export {DataServer} from './DataServer';
