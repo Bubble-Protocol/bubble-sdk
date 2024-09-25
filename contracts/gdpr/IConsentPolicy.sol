b663b650e647c9eba6007b06e56c4a13bd195773
Dave.Potter <dave@datonalabs.org>
2024-09-25 15:36:39 +0100

    fix incorrect header in IConsentPolicy

--- contracts/gdpr/IConsentPolicy.sol
+++ contracts/gdpr/IConsentPolicy.sol
@@ -4,14 +4,17 @@ pragma solidity ^0.8.0;
 
 
 /**
- * Contract to manage user consent. Stores up to 256 consent flags, any of which can be mandatory.
- * Application layer is responsible for what those flags represent and for informing the user.
+ * Contract to define a consent policy. A consent policy is a set of rules that define the
+ * minimum valid consent for a given application. The application layer is responsible for
+ * informing the user what the consent bits represent.
+ *
+ * Consent is stored as a bitfield, with each bit representing a different consent flag.
  */
 interface IConsentPolicy {
 
     /**
-     * @dev Returns true if the given consent meets the minimal mandatory consent requirements
-     * for this policy.
+     * @dev Returns true if the given consent bitfield meets the minimal mandatory consent 
+     * requirements for this policy.
      */
     function meetsMandatoryConsent(bytes32 _consent) external view returns (bool);
 
